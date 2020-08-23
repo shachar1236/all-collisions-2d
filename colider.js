8a810ae9362889e669bdb7fbdb83b9480069f0f5
shachar1236 <shachar5cohen@gmail.com>
2020-08-23 19:00:35 +0300

    Made polygon colider

--- colider.js
+++ colider.js
@@ -1,24 +1,77 @@
 class Colider {
-    static types = ["Box", "Circle", "Polygone"];
-
-    constructor(x, y) {
-        this.pos = createVector(x, y);
-        this.color = color(255, 0, 0);
-        this.center;
+    constructor(vertecis) {
+        this.vertecis = vertecis;
+        this.coliding = false;
     }
 
     move() {
-        let d = dist(mouseX, mouseY, this.pos.x, this.pos.y);
+        let d = dist(mouseX, mouseY, this.vertecis[0].x, this.vertecis[0].y);
         if (mouseIsPressed && d < 10) {
-            this.pos.x = mouseX;
-            this.pos.y = mouseY;
-            this._moved();
+            let disX = mouseX - this.vertecis[0].x;
+            let disY = mouseY - this.vertecis[0].y;
+            for (let i = 0; i < this.vertecis.length; i++) {
+                this.vertecis[i].x += disX;
+                this.vertecis[i].y += disY;
+            }
         }
     }
 
-    _moved() {}
+    add(x, y) {
+        for (let i = 0; i < this.vertecis.length; i++) {
+            this.vertecis[i].x += x;
+            this.vertecis[i].y += y;
+        }
+    }
 
     colide() {}
 
-    show() {}
+    show() {
+        beginShape();
+        if (this.coliding) {
+            stroke(0, 255, 0);
+        } else {
+            stroke(255, 0, 0);
+        }
+        noFill();
+        for (let ver of this.vertecis) {
+            vertex(ver.x, ver.y);
+        }
+        vertex(this.vertecis[0].x, this.vertecis[0].y);
+        endShape();
+    }
+
+    rotate(angle) {
+        let p = [[], []];
+        let n = this.vertecis.length;
+        let a = 0;
+        let b = 0;
+        for (let i = 0; i < n; i++) {
+            p[0][i] = this.vertecis[i].x;
+            p[1][i] = this.vertecis[i].y;
+            a += this.vertecis[i].x;
+            b += this.vertecis[i].y;
+        }
+        a *= 1 / n;
+        b *= 1 / n;
+
+        let c = [[], []];
+        for (let i = 0; i < n; i++) {
+            c[0][i] = a;
+            c[1][i] = b;
+        }
+
+        const r = [
+            [cos(angle), -sin(angle)],
+            [sin(angle), cos(angle)],
+        ];
+
+        let pNew;
+        pNew = math.subtract(p, c);
+        pNew = math.multiply(r, pNew);
+        pNew = math.add(pNew, c);
+        for (let i = 0; i < n; i++) {
+            this.vertecis[i].x = pNew[0][i];
+            this.vertecis[i].y = pNew[1][i];
+        }
+    }
 }
--- sketch.js
+++ sketch.js
@@ -3,19 +3,17 @@ let colider2;
 
 function setup() {
     createCanvas(400, 400);
-    colider1 = new Circle(10, 10, 60);
-    colider2 = new Box(100, 100, 40, 40);
+    colider1 = new Colider([createVector(0, 0), createVector(40, 0), createVector(40, 40), createVector(0, 40)]);
+    colider1.add(10, 20);
+    colider2 = new Colider([createVector(0, 0), createVector(40, 0), createVector(40, 40), createVector(0, 40)]);
+    colider2.add(100, 100);
+    colider2.rotate(0.5);
 }
 
 function draw() {
     background(0);
     colider1.move();
     colider2.move();
-    colider2.show();
     colider1.show();
-    if (colider1.colide(colider2)) {
-        colider1.color = color(0, 255, 0);
-    } else {
-        colider1.color = color(255, 0, 0);
-    }
+    colider2.show();
 }
